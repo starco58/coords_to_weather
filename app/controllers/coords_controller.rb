a7c72a2ae1ceb799d17383231da43d824f52ab29
Todd S <starco58@gmail.com>
2014-05-01 21:53:51 -0500

    entered URL and key
    no errors yet

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -5,12 +5,12 @@ class CoordsController < ApplicationController
   def fetch_weather
     @latitude = 42.0538387
     @longitude = -87.67721
-    your_api_key = ""
+    your_api_key = "2e4265c5279b734f4e9bb29d3786bf9a"
 
     # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
+    url = "https://api.forecast.io/forecast/2e4265c5279b734f4e9bb29d3786bf9a/#{@latitude},#{@longitude}"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
     # @temperature = ?
     # @minutely_summary = ?
     # @hourly_summary = ?
