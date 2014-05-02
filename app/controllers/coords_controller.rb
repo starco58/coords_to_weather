8fd90511337d909d6f0822bcb2e66acad16208e2
Todd S <starco58@gmail.com>
2014-05-01 22:14:15 -0500

    trying to id right variables
    not working....

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -11,7 +11,7 @@ class CoordsController < ApplicationController
     url = "https://api.forecast.io/forecast/2e4265c5279b734f4e9bb29d3786bf9a/#{@latitude},#{@longitude}"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
-    # @temperature = ?
+    @temperature = parsed_data["currently"][7]["temperature"]
     # @minutely_summary = ?
     # @hourly_summary = ?
     # @daily_summary = ?
