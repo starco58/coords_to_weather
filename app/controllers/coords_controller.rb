bcbe87d032d935705fbd08df4db8242367148884
Todd S <starco58@gmail.com>
2014-05-02 21:16:43 -0500

    figured out the weather array for current

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -8,10 +8,11 @@ class CoordsController < ApplicationController
     your_api_key = "2e4265c5279b734f4e9bb29d3786bf9a"
 
     # Your code goes here.
-    url = "https://api.forecast.io/forecast/2e4265c5279b734f4e9bb29d3786bf9a/#{@latitude},#{@longitude}"
+    url = "https://api.forecast.io/forecast/#{your_api_key}/#{@latitude},#{@longitude}"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
-    @temperature = parsed_data["currently"][7]["temperature"]
+    @temperature = parsed_data["currently"]["temperature"]
+
     # @minutely_summary = ?
     # @hourly_summary = ?
     # @daily_summary = ?
