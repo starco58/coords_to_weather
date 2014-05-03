cb75ea8233eedead8fd1d64519ce137026a7f683
Todd S <starco58@gmail.com>
2014-05-02 21:46:56 -0500

    phase 2 - trying to add form

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -1,10 +1,24 @@
 require 'open-uri'
 require 'json'
 
+class AddressesController < ApplicationController
+  def fetch_coordinates
+    @address = "the corner of foster and sheridan"
+    @url_safe_address = URI.encode(@address)
+
+    # Your code goes here.
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=false"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+  end
+end
+
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 42.0538387
-    @longitude = -87.67721
+    # @latitude = 42.0538387
+    # @longitude = -87.67721
     your_api_key = "2e4265c5279b734f4e9bb29d3786bf9a"
 
     # Your code goes here.
